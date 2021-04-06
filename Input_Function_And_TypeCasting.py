9367f40383283f8bfb8821189b7bf7476d7f27d6
Suraj-92 <suraj.temkar.92@gmail.com>
2021-04-06 21:01:02 +0530

    Added: Input Function And TypeCasting

---
+++ Input_Function_And_TypeCasting.py
@@ -0,0 +1,9 @@
+'''
+Author: Suraj N Temkar.
+Date: 06/04/2021
+Title: Demo of the Input Function And Type Casting.
+'''
+
+a = input("Enter a number: ")
+a = int(a) # Convert a to an Integer
+print(type(a))
