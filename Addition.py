8a5a1d389be2ab53ca833a47aea02594ad2ef5e7
Suraj-92 <suraj.temkar.92@gmail.com>
2021-04-06 21:05:30 +0530

    Refactored: Remainder calculated.

--- Addition.py
+++ Addition.py
@@ -1,9 +1,10 @@
 '''
 Author: Suraj N Temkar.
 Date: 06/04/2021
-Title: Addition of two Numbers.
+Title: Addition and Remainder of two Number.
 '''
 
 a = 30
 b = 15
 print("The sum of a and b is", a + b)
+print("The remainder when a is divided by b is", a%b)
