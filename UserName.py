aeeddbe06e91a5d51a99535f7c307c2b4987ee7b
Suraj-92 <suraj.temkar.92@gmail.com>
2021-04-06 20:34:26 +0530

    Refactored: Use Regex for UserName Validation

--- UserName.py
+++ UserName.py
@@ -4,10 +4,10 @@ Date: 05/04/2021
 Title: User Input and Replace String Template “Hello <<UserName>>, How are you?”
 '''
 
+import re
 username = input("Enter the Username: ")
-a = len(username)
 
-if (a > 3) :
+if re.match("^[A-Z][a-z]{2,}$", username):
     print("Username is valid")
     b = "Hello <<username>> How are you?"
     c = b.replace("<<username>>", username)
