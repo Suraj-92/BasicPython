f74f1f6f9f876a4da474833cfa7491e149019ad6
Suraj-92 <suraj.temkar.92@gmail.com>
2021-04-06 21:08:25 +0530

    Refactored: Average calculation

--- Addition.py
+++ Addition.py
@@ -1,10 +1,13 @@
 '''
 Author: Suraj N Temkar.
 Date: 06/04/2021
-Title: Addition and Remainder of two Number.
+Title: Addition of two Number , calculate Remainder And Average.
 '''
 
 a = 30
 b = 15
 print("The sum of a and b is", a + b)
 print("The remainder when a is divided by b is", a%b)
+
+avg = (a + b)/2
+print("The average of a and b is", avg)
