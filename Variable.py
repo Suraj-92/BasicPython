ac932142bc56a27d63e90f5141fc5714e1d948a8
Suraj-92 <suraj.temkar.92@gmail.com>
2021-04-06 20:51:28 +0530

    Added: Types Of Variables

---
+++ Variable.py
@@ -0,0 +1,25 @@
+'''
+Author: Suraj N Temkar.
+Date: 06/04/2021
+Title: Demo of the Types of the Python Variables
+'''
+
+a_122 = '''Suraj'''
+# a = 'Suraj'
+# a = "Suraj"
+b = 343
+c = 45.32
+d = True
+# d = None
+
+# Printing the variables
+print(a)
+print(b)
+print(c)
+print(d)
+
+# Printing the type of variables
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
