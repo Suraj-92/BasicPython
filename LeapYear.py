6ba205b9b72bfdef980cfcac2769bd616b6cf4df
Suraj-92 <suraj.temkar.92@gmail.com>
2021-04-07 23:38:05 +0530

    Refactor: Exception Handling And Main Function

--- LeapYear.py
+++ LeapYear.py
@@ -4,21 +4,30 @@ Date: 05/04/2021
 Title: Check year is Leap Year or Not.
 '''
 
-year = int(input("Enter 4-digit year: "))
-isLeap = False
-
-if(year%4 == 0):
-    if(year% 100 == 0):
-        if(year%400 == 0):
-            isLeap = True
+def leapYear():
+    try:
+        isLeap = False
+        if(year%4 == 0):
+            if(year% 100 == 0):
+                if(year%400 == 0):
+                    isLeap = True
+                else:
+                    isLeap = False
+            else:
+                isLeap = True
         else:
             isLeap = False
-    else:
-        isLeap = True
-else:
-    isLeap = False
 
-if(isLeap == True):
-    print(f"{year} is leap year")
-else:
-    print(f"{year} is not leap year")
+        if(isLeap == True):
+            print(f"{year} is leap year")
+        else:
+            print(f"{year} is not leap year")
+    except Exception as e:
+        print("Exception is: ", e)
+
+if __name__ == '__main__':
+    try:
+        year = int(input("Enter 4-digit year: "))
+    except Exception as e:
+        print(f"Exception occurs.....{e}.....Please enter 4-Digit Number")
+    leapYear()
