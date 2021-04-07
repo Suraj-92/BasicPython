9f78791e8d35fa28aae1207db925d2b50ea387fc
Suraj-92 <suraj.temkar.92@gmail.com>
2021-04-08 00:46:34 +0530

    Refactore: Nameing Convention

--- UserName.py
+++ UserName.py
@@ -4,21 +4,24 @@ Date: 05/04/2021
 Title: User Input and Replace String Template “Hello <<UserName>>, How are you?”
 '''
 
-import re
+import re    # import Regular Expression Library
 
-def validation():
-    try:
-        if re.match("^[A-Z][a-z]{2,}$", username):
+# Function
+def validation(username):
+    try:                                                           # Try Block
+        if re.match("^[A-Z][a-z]{2,}$", username):                 # Regex Pattern
             print("Username is valid",username)
-            b = "Hello <<username>> How are you?"
-            c = b.replace("<<username>>", username)
+            b = "Hello <<username>> How are you?"               
+            c = b.replace("<<username>>", username)                # Replace method
             print(c)
         else:
             raise Exception
     except Exception:
         print("Invalid input Please Enter The Proper Username ")
+    finally:                                                        # Finally block
+        print("......................")
 
+#Main Method
 if __name__ == '__main__':
-    username = input("Enter the Username: ")
-    validation()
-
+    username1 = input("Enter the Username: ")  # User Input
+    validation(username1)                      # Function Call
