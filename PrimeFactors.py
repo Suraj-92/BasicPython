27e6c247bc7a3360b004546bcec964055be090c3
Suraj-92 <suraj.temkar.92@gmail.com>
2021-04-08 00:53:57 +0530

    Refactore: Exception Handling and main Method and Nameing Convention

--- PrimeFactors.py
+++ PrimeFactors.py
@@ -4,17 +4,27 @@ Date: 05/04/2021
 Title: Computes the prime factorization of N using brute force.
 '''
 
+# Function Definition
 def prime_fact(x):
-    prime_factors = []
-    divisor = 2
-    while divisor <= x:
-        if x%divisor == 0:
-            prime_factors.append(divisor)
-            x = x/divisor
-        else:
-            divisor += 1 
-    return prime_factors
+    try:                # Try Block
+        prime_factors = []
+        divisor = 2
+        while divisor <= x:
+            if x%divisor == 0:
+                prime_factors.append(divisor)
+                x = x/divisor
+            else:
+                divisor += 1 
+        return prime_factors
+    except Exception as e:         # except Block
+        print("Exception is:", e)
 
-number = int(input("Enter a number to get its prime factors: "))
-a = prime_fact(number)
-print(f"Prime Factors of {number} is : {a}")
+# Main Method
+if __name__ == '__main__':
+    try:
+        number = int(input("Enter a number to get its prime factors: "))     # User Input
+        prime_fact(number)   # Function Call
+        print(f"Prime Factors of {number} is : {prime_fact(number)}")
+
+    except Exception as e:
+        print("Exception occur",e)
