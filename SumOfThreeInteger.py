2fb2a0dc77b4cca18cac2b3c596353b1175997a0
Suraj-92 <suraj.temkar.92@gmail.com>
2021-04-07 09:15:08 +0530

    Added: Sum Of Three Integer Adds To Zero

---
+++ SumOfThreeInteger.py
@@ -0,0 +1,23 @@
+''' 
+Author: Suraj N Temkar.
+Date: 07/04/2021
+Title: A program with cubic running time. Read in N integers and counts the   number of triples that sum to exactly 0.
+'''
+
+def sumOfThree(arr):
+    a = len(arr)
+    for i in range(0, a):
+        firstnumber = arr[i]
+        for j in range(i+1, a):
+            secondnumber = arr[j]
+            for k in range(j+1):
+                thirdnumber = arr[k]
+
+                sum = firstnumber + secondnumber + thirdnumber
+                if(sum == 0):
+                    print(f"{firstnumber}, {secondnumber}, {thirdnumber}")
+
+arr = [3, -1, -7, -4, -5, 9, -4]
+function = sumOfThree(arr)
+print(function)
+                
