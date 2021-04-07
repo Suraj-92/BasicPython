da4cad569259b691dc2afce99056af382ebaf818
Suraj-92 <suraj.temkar.92@gmail.com>
2021-04-07 23:47:17 +0530

    Refactor: Exception Handling And Main Function

--- PowerOf2.py
+++ PowerOf2.py
@@ -5,6 +5,17 @@ Title: This program takes a command-line argument N and prints a table of the po
 '''
 
 import math
-number = int(input("Enter a number to print power of 2 till Nth terms: "))
-a = math.pow(2, number)
-print(a)
+
+def power():
+    try:
+        powerOf2 = math.pow(2, number)
+        print(powerOf2)
+    except Exception as e:
+        print(f"Exception Occurs.....{e} Please Enter the proper Input")
+
+if __name__ == '__main__':
+    try:
+        number = int(input("Enter a number to print power of 2 till Nth terms: "))
+    except Exception as e:
+        print(f"Exception Occurs.....{e} Please enter the Integer")
+    power()
