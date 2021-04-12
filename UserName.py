3504654f6039c21722c1d226b4014080f16218ea
Suraj-92 <suraj.temkar.92@gmail.com>
2021-04-12 12:14:44 +0530

    Refactor: Add Logger in username Problem from Importing Logger File

--- UserName.py
+++ UserName.py
@@ -5,21 +5,34 @@ Title: User Input and Replace String Template “Hello <<UserName>>, How are you
 '''
 
 import re    # import Regular Expression Library
+import Logger  # import Logger Module 
+import logging      # import logging library
+
+logger = logging.getLogger(__name__)
+
+logger.setLevel(logging.INFO)
+
+formater = logging.Formatter('%(levelname)s : %(message)s')
+
+file_handler = logging.FileHandler('sample2.log')
+file_handler.setFormatter(formater)
+
+logger.addHandler(file_handler)
 
 # Function
 def validation(username):
     try:                                                           # Try Block
         if re.match("^[A-Z][a-z]{2,}$", username):                 # Regex Pattern
-            print("Username is valid",username)
+            logger.info("Username is valid",username)
             b = "Hello <<username>> How are you?"               
             c = b.replace("<<username>>", username)                # Replace method
-            print(c)
+            logger.info(c)
         else:
             raise Exception
     except Exception:
-        print("Invalid input Please Enter The Proper Username ")
+        logger.info("Invalid input Please Enter The Proper Username ")
     finally:                                                        # Finally block
-        print("......................")
+        logger.info("......................")
 
 #Main Method
 if __name__ == '__main__':
