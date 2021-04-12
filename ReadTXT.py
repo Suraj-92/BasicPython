5ea89aa6ce8c185270ed3fdc9f57684e62aed2c8
Suraj-92 <suraj.temkar.92@gmail.com>
2021-04-12 13:30:20 +0530

    Refactor: Add Updated file

--- ReadTXT.py
+++ ReadTXT.py
@@ -1,31 +1,20 @@
 '''
 Author: Suraj N Temkar
 Date: 12/04/2021
-Title: File IO (Write The File)
+Title: File IO (Read The File)
 '''
 
-f = open("C:\\Users\\DELL\\Desktop\\Python\\FileIO\File1.txt", "a")
-f.write("Now the file has more content!")
-f.close()
-
-#open and read the file after the appending
 f = open("C:\\Users\\DELL\\Desktop\\Python\\FileIO\File1.txt", "r")
-print(f.read())
+# print(f.read())   # Read File Contents
+
+# print(f.read(5))    # Reads Only Parts of file
+
+# print(f.readline())     # Reads Single Line
+# print(f.readline())     
 
 
-# Open the file "File1.txt" and overwrite the content
-# f = open("C:\\Users\\DELL\\Desktop\\Python\\FileIO\File1.txt", "w")
-# f.write("Woops! I have deleted the content!")
-# f.close()
+# Loop through the file line by line
+for x in f:
+  print(x)
 
-# #open and read the file after the appending
-# f = open("C:\\Users\\DELL\\Desktop\\Python\\FileIO\File1.txt", "r")
-# print(f.read())
-
-
-# Create a file called "myfile.txt":
-# f = open("File2.txt", "x")
-
-# Create a new file if it does not exist:
-# f = open("myfile.txt", "w")
-
+f.close()       # Close File
